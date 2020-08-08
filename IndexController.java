976edc16fa974265d41c6c649b1ef006f1fd7b7f
Studiedlist <49450452+Studiedlist@users.noreply.github.com>
2020-08-08 13:14:55 +0300

    Add files via upload

---
+++ IndexController.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class IndexController {
+
+    @RequestMapping(value= "/", method= RequestMethod.GET)
+    String main() {
+        return "templates/index.mustache";
+    }
+
+    @RequestMapping(value= "/error", method= RequestMethod.GET) 
+    ResponseEntity<String> error() {
+    	return new ResponseEntity<>("There is a error!", HttpStatus.OK);
+    }
+    
+}
