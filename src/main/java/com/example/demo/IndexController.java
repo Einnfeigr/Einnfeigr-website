bfc8a392c57ee74cfe24080a4970999da342ee01
Studiedlist <49450452+Studiedlist@users.noreply.github.com>
2020-08-08 13:09:56 +0300

    Add files via upload

--- src/main/java/com/example/demo/IndexController.java
+++ src/main/java/com/example/demo/IndexController.java
@@ -5,12 +5,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-
 public class IndexController {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public ResponseEntity<String> type(@RequestBody String req) {
+    @RequestMapping(value= "/", method= RequestMethod.GET)
+    ResponseEntity<String> main() {
         return new ResponseEntity<>("hello world!", HttpStatus.OK);
     }
 
+    @RequestMapping(value= "/error", method= RequestMethod.GET) 
+    ResponseEntity<String> error() {
+    	return new ResponseEntity<>("There is a error!", HttpStatus.OK);
+    }
+    
 }
