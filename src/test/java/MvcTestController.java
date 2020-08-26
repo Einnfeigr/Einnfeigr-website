1cfea6444b31d8fa18dd1595c1cfb0562dd1bef4
Studiedlist <49450452+Studiedlist@users.noreply.github.com>
2020-08-26 23:32:59 +0300

    Update test

--- src/test/java/MvcTestController.java
+++ src/test/java/MvcTestController.java
@@ -3,6 +3,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -12,6 +13,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import main.PageController;
+import main.SecurityController;
 import main.misc.configuration.ProjectConfiguration;
 import main.misc.configuration.SecurityConfiguration;
 
@@ -19,10 +21,16 @@ import main.misc.configuration.SecurityConfiguration;
 @WebMvcTest(PageController.class)
 @ContextConfiguration(
 		classes= {ProjectConfiguration.class, SecurityConfiguration.class})
+@ImportResource({ "classpath:applicationContext.xml" })
 public class MvcTestController {
 
+	private static final String MOBILE_USER_AGENT = "Mozilla/5.0 (Linux; Android 7.1; Mi A1 Build/N2G47H";
+	
 	@Autowired
 	PageController pageController;
+	
+	@Autowired
+	SecurityController securityController;
 	
 	@Autowired
     private MockMvc mvc;
@@ -31,10 +39,25 @@ public class MvcTestController {
 	public void testPageController() throws Exception {
 		mvc.perform(MockMvcRequestBuilders
 				.get("/")
-				.header("user-agent", "Mozilla/5.0 (Linux; Android 7.1; Mi A1 Build/N2G47H")
+				.header("user-agent", MOBILE_USER_AGENT)
 				.accept(MediaType.ALL))
 				.andDo(MockMvcResultHandlers.print())
 				.andExpect(MockMvcResultMatchers.status().isOk());
+	}
+	
+	@Test
+	public void testSecurityController() throws Exception {
+		mvc.perform(MockMvcRequestBuilders
+				.get("login")
+				.header("user-agent", MOBILE_USER_AGENT)
+				.accept(MediaType.ALL))
+				.andDo(MockMvcResultHandlers.print())
+				.andExpect(MockMvcResultMatchers.status().isOk());
+		mvc.perform(MockMvcRequestBuilders
+				.get("dashboard")
+				.header("user-agent", MOBILE_USER_AGENT)
+				.accept(MediaType.ALL))
+				.andExpect(MockMvcResultMatchers.status().isUnauthorized());
 	}
 	
 }
--- src/test/java/unit/TemplateTestController.java
+++ src/test/java/unit/TemplateTestController.java
@@ -1,27 +1,10 @@
 package unit;
 
-import java.io.IOException;
-
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
-import main.pojo.TemplateData;
-import main.template.TemplateController;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 public class TemplateTestController {
 
-	@Test
-	public void testCompileTemplate() throws IOException {
-		@SuppressWarnings("unused")
-		TemplateData data = new TemplateData() {
-			String text = "test";
-		};
-		assertThat(TemplateController.compileTemplate("test", data)
-				.equals("<div class=\"test\">test</div>"));
-	}
 	
 }
