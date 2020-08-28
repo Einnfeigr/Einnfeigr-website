28c13133a3cef6fd89487ffa2768c9d79a451c0d
studiedlist <studiedlist@192.168.5.129>
2020-08-28 14:06:36 +0300

    Establish security test

--- src/test/java/SecurityTest.java
+++ src/test/java/SecurityTest.java
@@ -3,13 +3,13 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import main.PageController;
 import main.SecurityController;
 import main.misc.configuration.ProjectConfiguration;
 import main.misc.configuration.SecurityConfiguration;
@@ -47,20 +47,10 @@ public class SecurityTest {
 				.andExpect(MockMvcResultMatchers.status().isOk());
 	}
 	
-	@Test
-	public void login() throws Exception {
-		mvc.perform(MockMvcRequestBuilders
-				.get("/login")
-				.header("user-agent", MOBILE_USER_AGENT)
-				.accept(MediaType.ALL))
-				.andExpect(MockMvcResultMatchers.status().isOk());
 
-		mvc.perform(MockMvcRequestBuilders
-				.get("/login?username=studiedlist&password=A3a2qZbxFF")
-				.header("user-agent", "MOBILE_USER_AGENT")
-				.accept(MediaType.ALL))
-				.andExpect(MockMvcResultMatchers.status().isOk());
-		
+	@WithMockUser(value = "studiedlist")
+	@Test
+	public void dashboard() throws Exception {
 		mvc.perform(MockMvcRequestBuilders
 				.get("/dashboard")
 				.header("user-agent", MOBILE_USER_AGENT)
