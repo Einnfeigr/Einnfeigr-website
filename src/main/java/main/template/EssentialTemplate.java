9a504201c25b977d9ff2e8698f915773bb9141e2
Studiedlist <studiedlist@gmai.com>
2020-09-04 17:24:48 +0300

    update EssentialTemplate

--- src/main/java/main/template/EssentialTemplate.java
+++ src/main/java/main/template/EssentialTemplate.java
@@ -1,7 +1,5 @@
 package main.template;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.StringWriter;
 
@@ -9,7 +7,6 @@ import com.github.mustachejava.DefaultMustacheFactory;
 import com.github.mustachejava.Mustache;
 import com.github.mustachejava.MustacheFactory;
 
-import main.misc.Util;
 import main.template.data.TemplateData;
 
 public class EssentialTemplate implements Template {
@@ -55,19 +52,6 @@ public class EssentialTemplate implements Template {
 	}
 
 	public String compile() throws IOException {
-		if(templatePath == null) {
-			throw new IllegalArgumentException(
-					"Template path must be not null");
-		}
-	   	File template = Util.getFile(templatePath);
-	   	if(!template.exists()) {
-	   		throw new FileNotFoundException(
-	   				"File '"+templatePath+"' cannot be found");
-	   	}
-	   	if(!template.isFile()) {
-	   		throw new IllegalArgumentException(
-	   				"Template '"+templatePath+"' must be a file");
-	   	}
 	   	MustacheFactory factory = new DefaultMustacheFactory();
 	   	Mustache mustache = factory.compile(templatePath);
 	   	StringWriter writer = new StringWriter();
