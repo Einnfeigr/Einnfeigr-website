f1e3746a39eec90a0fa469c14d75446a5d4fbe24
Studiedlist <studiedlist@gmai.com>
2020-09-23 18:38:13 +0300

    update CallbackReciever

--- src/main/java/main/CallbackReciever.java
+++ src/main/java/main/CallbackReciever.java
@@ -3,6 +3,8 @@ package main;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,9 +26,10 @@ public class CallbackReciever {
 	AlbumController albumController;
 	
 	@RequestMapping("api/drive/callback")
-	public void handleCallback() {
+	public ResponseEntity<String> handleCallback() {
 		portfolioDriveDao.clearCache();
 		previewDriveDao.clearCache();
 		albumController.loadAlbums();
+		return new ResponseEntity<String>("", HttpStatus.OK);
 	}
 }
